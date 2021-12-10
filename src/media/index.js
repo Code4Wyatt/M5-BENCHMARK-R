dcde1a56287f8595c0bfafdd4769c6ae108728fd
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-12-10 11:01:33 +0000

    Update

--- src/media/index.js
+++ src/media/index.js
@@ -10,3 +10,52 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const mediaRouter = express.Router();
+
+mediaRouter.get("/", async (req, res, next) => {
+    try {
+        const fileAsBuffer = fs.readFileSync(mediaFilePath);
+        const fileAsString = fileAsBuffer.toString();
+        const fileAsJSON = JSON.parse(fileAsString);
+        res.send(fileAsJSON);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+mediaRouter.post("/", async (req, res, next) => {
+    try {
+        const media = {
+            id: uniqid(),
+            ...req.body,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        };
+
+        const fileAsBuffer = fs.readFileSync(mediaFilePath);
+        const fileAsString = fileAsBuffer.toString();
+        const fileAsJSONArray = JSON.parse(fileAsString);
+
+        fileAsJSONArray.push(media);
+
+        fs.writeFileSync(mediaFilePath, JSON.stringify(fileAsJSONArray));
+        res.send(media);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+});
+
+mediaRouter.get("/:id", async (req, res, next) => {
+    try {
+        const fileAsBuffer = fs.readFileSync(mediaFilePath);
+        const fileAsString = fileAsBuffer.toString();
+        const fileAsJSONArray = JSON.parse(fileAsString);
+
+        const media = fileAsJSONArray.find((media) => media.id === req.params.id);
+        if (!media) {
+            res.status(404).send({ message: `media with ${req.params.id} not found`})
+        }
+        res.send(media);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+})
